d306b3b09096040115ce077226444ce2b571043f
DanielZFLiu <daniel.zf.liu@gmail.com>
2023-07-09 13:57:06 -0400

    routes setup and metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'Epoch Design',
+      meta: [
+        {name: 'description', content: 'The Epoch Design landing page'}
+      ],
+    },
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        hey
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
