d67c7f93c8c04ad9beecca64b496a19dc44758cb
RobbyChen2001 <rforacademic@gmail.com>
2023-07-30 00:09:25 -0400

    added the contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,6 +2,9 @@
     <div class="main">
         <div class="canvas">
             <SubpageTitle title="Contact" subtitle="If you'd like to learn more about<br>how we can help evolve your brand, get in touch."/>
+            <div class="contact_form">
+                <ContactUsForm/>
+            </div>
         </div>
     </div>
 </template>
@@ -42,5 +45,33 @@
     bottom: 0;
     left: 0;
     right: 0;
+
+    /* interaction */
+    overflow-y: scroll;
+    overflow-x: hidden;
 }
+
+/* scrollbar width */
+::-webkit-scrollbar {
+    width: 10px;
+  }
+  
+  /* scrollbar Track */
+  ::-webkit-scrollbar-track {
+    background: var(--onyx-black);
+  }
+  
+  /* scrollbar Handle */
+  ::-webkit-scrollbar-thumb {
+    background: var(--dandelion-yellow);
+  }
+
+.contact_form{
+    /* position */
+    position: absolute;
+    top: 65vh;
+    left: 50%;
+    transform: translateX(-50%);
+}
+
 </style>
