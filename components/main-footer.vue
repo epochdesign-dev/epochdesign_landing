1005540c9b1454e14dfa0e0abb4ca749aea39c68
RobbyChen2001 <rforacademic@gmail.com>
2023-08-06 02:27:27 -0400

    implement basic  service page for web-design

---
+++ components/main-footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <footer class="main_footer">
+        </footer>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.main_footer{
+
+    /* shape */
+    width: 100vw;
+    height: 28.89vh;
+    border-top: solid 3px white;
+}
+
+@media screen and (max-width: 640px) {
+    .main_footer{
+        /* shape */
+        width: 95.9vw;
+        height: 43.33vh;
+        border-top: solid 3px white;
+    }
+}
+</style>
