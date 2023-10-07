2161395d461910e2d3d19f58ff3241ff465b346c
DanielZFLiu <daniel.zf.liu@gmail.com>
2023-10-07 10:20:13 -0400

    sitemap update 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss','nuxt-swiper', '@nuxtjs/supabase'],
+  modules: ['@nuxtjs/tailwindcss','nuxt-swiper', '@nuxtjs/supabase', "@nuxtjs/sitemap"],
   app: {
     head: {
       title: 'Epoch Design & Development',
@@ -24,5 +24,5 @@ export default defineNuxtConfig({
     url: "https://rdlfeqgerdgwbzbofycd.supabase.co",
     key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkbGZlcWdlcmRnd2J6Ym9meWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg5MjYwNzIsImV4cCI6MjAwNDUwMjA3Mn0.gmiH4xpkFgKiRobo7WDjgpemOfcZfrOwTZL4HpmhGrY",
     redirect: false,
-  }
+  },
 })
