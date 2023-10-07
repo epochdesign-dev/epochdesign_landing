b7da0378254ec97cc2954e4d39a600224b9cfda9
DanielZFLiu <daniel.zf.liu@gmail.com>
2023-10-07 10:47:11 -0400

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss','nuxt-swiper', '@nuxtjs/supabase', "@nuxtjs/sitemap"],
+  modules: ['@nuxtjs/tailwindcss','nuxt-swiper', '@nuxtjs/supabase'],
   app: {
     head: {
       title: 'Epoch Design & Development',
